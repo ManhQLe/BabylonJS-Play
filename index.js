e0944663f2b0a16d7d579a1dcc58a1b19619fb9c
Manh Le <quang.manh@gmail.com>
2017-11-09 20:29:17 -0600

    Add babylon svg

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import express from 'express';
-import path from 'path'
-//const express = require('express');
-//const path = require('path');
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8000;
 app.use(express.static(path.join(__dirname,'statics')));
