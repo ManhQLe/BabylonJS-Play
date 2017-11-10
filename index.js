a8608b5a2febad56005a5555dae02671144b4e15
Manh Le <quang.manh@gmail.com>
2017-11-09 21:48:09 -0600

    Replace html content holder

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
+const route = require('./router');
 
-
-
+app.use(route);
 app.use('/public',express.static(path.join(__dirname,'statics')));
 
 app.listen(port,function(){
--- router.js
+++ router.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const path = require('path')
+const async8 = require('async8');
 var router = express.Router()
 const fs = require('fs');
 
-router.get('/:scene',function(req,res){
-    var scenePath = path.join(__dirname,'public/scenes',req.params.scene+".js");
-    
-    fs.readFile(scenePath,function(err,data){
+router.get('/:scene', function (req, res) {
 
+    const htmlpath = path.join(__dirname, 'scene.html');
+    fs.readFile(htmlpath, 'utf8', function (err, data) {
+        if (err)
+            res.status(500).send(err)
+        else {
+            res
+            .type(".html")
+            .send(data.replace("[[SCENE]]",req.params.scene));
+        }
     })
 })
+
+
 
 module.exports = router;
