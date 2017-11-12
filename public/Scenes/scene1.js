9fb18fe2a205d8d77ac9159b9af4050fdbf55b7f
Manh Le <quang.manh@gmail.com>
2017-11-12 00:16:44 -0600

    Finish babylonJS getting started

--- public/Scenes/scene1.js
+++ public/Scenes/scene1.js
@@ -1,3 +1,35 @@
+var engine,canvas
+var vec3 = BABYLON.Vector3
+function start(en,c){
+    engine = en;
+    canvas = c;
+    render();
+}
+
+
+
+function createScene(){
+    var scene = new BABYLON.Scene(engine);
+    var cam = new BABYLON.FreeCamera('camera',new vec3(0,5,-10),scene);
+
+    cam.setTarget(vec3.Zero());
+
+    cam.attachControl(canvas,false)
+
+    var light = new BABYLON.HemisphericLight('light',new vec3(0,1,0),scene);
+
+    var sphere = new BABYLON.MeshBuilder.CreateSphere('sphere',{segments:16,diameter:2},scene)
+
+    sphere.position.y = 1;
+
+    var ground = BABYLON.Mesh.CreateGround('ground1',6,6,2,scene);
+
+    return scene
+}
+
 function render(){
-    console.log("Render");
+    var scene = createScene();
+    engine.runRenderLoop(function(){
+        scene.render();
+    })
 }
